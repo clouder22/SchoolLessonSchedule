62c4edf31748a988cc12c56ed25cf60d7934f09d
clouder22 <133132041+clouder22@users.noreply.github.com>
2024-11-18 10:07:30 +0000

    aef

--- main.js
+++ main.js
@@ -81,14 +81,14 @@ function registerSchedule(){
 
     setLessons(day,time,lesson)
 
-    document.getElementById("selectionTime").value = parseInt(document.getElementById("selectionTime").value) + 1;
+    document.getElementById("selectionTime").value = String(parseInt(document.getElementById("selectionTime").value) + 1);
 
     if(parseInt(document.getElementById("selectionTime").value) == 6){
-        document.getElementById("selectionTime").value = 0
-        document.getElementById("selectionDay").value = parseInt(document.getElementById("selectionDay").value) + 1
+        document.getElementById("selectionTime").value = "0"
+        document.getElementById("selectionDay").value = String(parseInt(document.getElementById("selectionDay").value) + 1)
         if(document.getElementById("selectionDay").value == 6){
-            document.getElementById("selectionDay").value = 0
-            document.getElementById("selectionTime").value = 0
+            document.getElementById("selectionDay").value = "0"
+            document.getElementById("selectionTime").value = "0"
         }
     }
 
